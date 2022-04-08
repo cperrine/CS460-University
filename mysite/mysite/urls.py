505e8298982b4de17501998bafc0de4afb88d0fc
cperrine <perrince@clarkson.edu>
2022-04-08 09:28:11 -0400

    f2 works

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('instructor/', views.instructor),
     path('admin/', admin.site.urls),
     path('index/', views.index),
-    path('administrator_f2/', views.administrator),
+    path('administrator_f2/', views.administrator_f2),
 ]
