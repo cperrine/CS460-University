1d0c570f0c16a41ee11fd9333d7d1d0c511cce50
Warvan1 <84527285+Warvan1@users.noreply.github.com>
2022-04-08 22:18:25 -0400

    Did part F6.
    I changed the student page so that when you visited it you could enter information into the form and then it would query with that according to specification F6.

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,9 +1,9 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 import mysql.connector
 from django.shortcuts import render
-#from django.views.decorators import csrf exempt
+from django.views.decorators.csrf import csrf_exempt
 
-
+my_passward = "" #put your passward here
 
 def index(request):
     form='<!DOCTYPE html>' +\
@@ -17,51 +17,76 @@ def index(request):
     '</form>' +\
     '</body>/' +\
     '</html>'
-    return HttpResponse('Hello, this is the index page')
-    
+    return HttpResponse(form)
+
+@csrf_exempt
 def student(request):
-    mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    passwd="Snaildog13!",    #USE YOUR PASSWORD
-    auth_plugin='mysql_native_password',
-    database="university",
-    )
-    
-    mycursor = mydb.cursor()
-    
-    mycursor.execute('select * from section;')
-    
-    data = '<table style="width:400px">'
-    for (course_id,sec_id,semester,year,building,room,capacity) in mycursor:
-        r= ('<tr>'+ \
-            '<th>' + course_id+'</th>'+\
-            '<th>' + str(sec_id) + '</th>'+\
-            '<th>' + str(semester) + '</th>'+\
-            '<th>' + str(year) + '</th>'+\
-            '<th>' + building + '</th>'+\
-            '<th>' + str(room) + '</th>'+\
-            '<th>' + str(capacity) + '</th>'+\
-            '</tr>')
-        data += r
-    data += '</table>'
-    mycursor.close()
-    mydb.close()
+    try:
+        department = request.POST['department']
+        year = request.POST['year']
+        semester = request.POST['semester']
+    except:
+        data='<!DOCTYPE html>' +\
+            '<html>' +\
+            '<body>' +\
+            '<h1>Student Form:</h1>'+\
+            '<form action="" method="post">' +\
+                '<label for="department">department: </label>' +\
+                '<input type="text" id="department" name="department"><br><br>' +\
+                '<label for="year">year: </label>' +\
+                '<input type="text" id="year" name="year"><br><br>' +\
+                '<label for="semester">semester: </label>' +\
+                '<input type="text" id="semester" name="semester"><br><br>' +\
+                '<input type="submit" value="Submit">' +\
+            '</form>' +\
+            '</body>/' +\
+            '</html>'
+    else:
+        mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        passwd=my_passward,
+        auth_plugin='mysql_native_password',
+        database="university",
+        )
+        mycursor = mydb.cursor()
+        
+        mycursor.execute(f'select section.course_id, course.title, course.credits, section.building, section.room, section.capacity  from course inner join section on course.course_id = section.course_id where dept_name = "{department}" and semester = {semester} and year = {year} group by section.course_id;')
+        
+        data = '<p>courseID, Title, Credits, Building, Room, Capacity' +\
+                '<table style="width:400px">'
+        for (course_id,title,credits,building,room,capacity) in mycursor:
+            r= ('<tr>'+ \
+                '<th>' + course_id+'</th>'+\
+                '<th>' + title + '</th>'+\
+                '<th>' + str(credits) + '</th>'+\
+                '<th>' + building + '</th>'+\
+                '<th>' + str(room) + '</th>'+\
+                '<th>' + str(capacity) + '</th>'+\
+                '</tr>')
+            data += r
+        data += '</table>'
+        mycursor.close()
+        mydb.close()
     return HttpResponse(data)
     
-#@csrf_exempt
+@csrf_exempt
 def instructor(request):
     mydb = mysql.connector.connect(
     host="localhost",
     user="root",
-    passwd="Snaildog13!",
+    passwd=my_passward,
     auth_plugin='mysql_native_password',
     database="university",
     )
     
     mycursor = mydb.cursor()
     
-    sal= 80000
+    try:
+        sal= request.POST['amount']
+    except:
+        sal=0
+
     query=f'SELECT * FROM instructor WHERE salary > {sal};'
     mycursor.execute(query)
     
@@ -78,11 +103,12 @@ def instructor(request):
     mycursor.close()
     mydb.close()
     return HttpResponse(data)
+
 def administrator_f2(request):
     mydb = mysql.connector.connect(
     host="localhost",
     user="root",
-    passwd="Snaildog13!",  #USE YOUR PASSWORD
+    passwd=my_passward,
     auth_plugin='mysql_native_password',
     database="university",
     )
