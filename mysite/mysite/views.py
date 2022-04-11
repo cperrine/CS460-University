2e01592de82480060d2aacd91db174d4e2e3b240
Warvan1 <84527285+Warvan1@users.noreply.github.com>
2022-04-11 00:14:37 -0400

    F5
    I Implemented Part F5

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -112,12 +112,82 @@ def instructor(request):
         data = '<table style="width:400px">'
         for (course_id,sec_id,building,room,capacity,students) in mycursor:
             r= ('<tr>'+ \
-                '<th>Course: ' + str(course_id) +'</th>'+\
+                '<th>Course:  ' + str(course_id) +'</th>'+\
                 '<th>Section: ' + str(sec_id) + '</th>'+\
                 '<th>Building: ' + building + '</th>'+\
                 '<th>Room: ' + str(room) + '</th>'+\
                 '<th>Capacity: ' + str(capacity) + '</th>'+\
                 '<th>Students: ' + str(students) + '</th>'+\
+                '</tr>')
+            data += r
+        data += '</table>'
+        mycursor.close()
+        mydb.close()
+
+        data += f'<h1>Section Form:</h1>'+\
+            '<form action="section" method="post">' +\
+                '<label for="instructor_id">Instructor ID: </label>' +\
+                '<input type="text" id="instructor_id" name="instructor_id" value="' + instructor_id + '"><br><br>' +\
+                '<label for="semester">Semester: </label>' +\
+                '<input type="text" id="semester" name="semester" value="' + semester + '"><br><br>' +\
+                '<label for="year">Year: </label>' +\
+                '<input type="text" id="year" name="year" value="' + year + '"><br><br>' +\
+                '<label for="course_id">Course: </label>' +\
+                '<input type="text" id="course_id" name="course_id"><br><br>' +\
+                '<label for="sec_id">section: </label>' +\
+                '<input type="text" id="sec_id" name="sec_id"><br><br>' +\
+                '<input type="submit" value="Submit">' +\
+            '</form>' +\
+            '</body>/' +\
+            '</html>'
+    return HttpResponse(data)
+
+@csrf_exempt
+def section(request):
+    try:
+        instructor_id = request.POST['instructor_id']
+        semester = request.POST['semester']
+        year = request.POST['year']
+        course_id = request.POST['course_id']
+        sec_id = request.POST['sec_id']
+    except:
+        data = f'<h1>Section Form:</h1>'+\
+            '<form action="section" method="post">' +\
+                '<label for="instructor_id">Instructor ID: </label>' +\
+                '<input type="text" id="instructor_id" name="instructor_id"><br><br>' +\
+                '<label for="semester">Semester: </label>' +\
+                '<input type="text" id="semester" name="semester"><br><br>' +\
+                '<label for="year">Year: </label>' +\
+                '<input type="text" id="year" name="year"><br><br>' +\
+                '<label for="course_id">Course: </label>' +\
+                '<input type="text" id="course_id" name="course_id"><br><br>' +\
+                '<label for="sec_id">section: </label>' +\
+                '<input type="text" id="sec_id" name="sec_id"><br><br>' +\
+                '<input type="submit" value="Submit">' +\
+            '</form>' +\
+            '</body>/' +\
+            '</html>'
+    else:
+        mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        passwd=my_passward,
+        auth_plugin='mysql_native_password',
+        database="university",
+        )
+        
+        mycursor = mydb.cursor()
+
+        query=f'select s.ID, s.name, s.dept_name, s.tot_cred from (select t1.course_id, t1.sec_id, t1.building, t1.room, t1.capacity, t2.ID from (select s.course_id, s.sec_id, s.semester, s.year, s.building, s.room, s.capacity from section s inner join teaches t on s.course_id = t.course_id and s.sec_id = t.sec_id and s.semester = t.semester and s.year = t.year where t.id = {instructor_id} and t.semester = {semester} and t.year = {year} and t.course_id = "{course_id}" and t.sec_id = {sec_id}) as t1 left join takes t2 on t1.course_id = t2.course_id and t1.sec_id = t2.sec_id and t1.semester = t2.semester and t1.year = t2.year) as t3 join student s on t3.ID = s.ID;'
+        mycursor.execute(query)
+        
+        data = '<table style="width:400px">'
+        for (id,name,dept_name,tot_cred) in mycursor:
+            r= ('<tr>'+ \
+                '<th>ID: ' + str(id) +'</th>'+\
+                '<th>Name: ' + name + '</th>'+\
+                '<th>Department: ' + dept_name + '</th>'+\
+                '<th>Total Credits: ' + str(tot_cred) + '</th>'+\
                 '</tr>')
             data += r
         data += '</table>'
